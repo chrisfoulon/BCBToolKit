97142fc153cec30faa8619243fe6664251e5d775
Chris Foulon <hd.chrisfoulon@gmail.com>
2015-12-22 13:53:13 +0100

    Compiling modifications and test for accessing to nifti images, even compressed

--- Sources/bcbnifti/main.c
+++ Sources/bcbnifti/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "znzlib/znzlib.h"
+#include "fslio/fslio.h"
+#include "niftiio/nifti1_io.h"
+
 
 int main(int argc, char **argv) {
     int fd = open( "testni", O_RDWR );
@@ -9,10 +12,30 @@ int main(int argc, char **argv) {
     znzFile* p = &z;
     Xznzclose(p);
     
+    FSLIO *fslio = FslInit();
+    /*const char* fname = "/home/tolhs/Tractotron/BCBToolKit/Lesions/lesionpatient1.nii.gz";
+    char* varname = "/home/tolhs/MesDocuments/normalize/RES.nii";
+    varname = fname;*/
+    char* varname = "/home/tolhs/MesDocuments/normalize/RES.nii.gz";
     
+    if (nifti_validfilename(varname)) {
+	printf("nifti_validfilename(fname) OK.\n");
+    }
+    FslReadAllVolumes(fslio, varname);
+    printf("OK \n");
+    //FslClose(fslio);
+    //printf("OK2 \n");
     
+    printf("DataType : %d\n", fslio->niftiptr->datatype);
+    printf("Sizeof int : %lu\n", sizeof(double));
+    printf("Datatype to string : %s\n", nifti_datatype_to_string(fslio->niftiptr->datatype));
     
+    double*** mat = FslGetVolumeAsScaledDouble(fslio, 0);
     
-    printf("Hello world\n");
+    for (int i = 0; i < 50; i++) {
+      printf("[%f]", mat[i][i][i]);
+    }
+    
+    printf("\nHello world\n");
     return EXIT_SUCCESS;
 }

12121e63bf93501f46c7ec2f7c775f121bd45553
Chris Foulon <hd.chrisfoulon@gmail.com>
2015-12-22 13:54:51 +0100

    Cleaning C files and adding initFSLEnv.sh, a command file to export env variables

---
+++ Sources/bcbnifti/initFSLEnv.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+path=/home/tolhs/Tractotron/BCBToolKit/Tools
+lib=$path/libraries/lib
+bin=$path/binaries/bin
+ants=$path/binaries/ANTs
+export FSLDIR=$path/binaries
+#This line prevent missing of the bc binary for ANTs
+export PATH=$PATH:$path/binaries/bin
+
+export LD_LIBRARY_PATH=$lib
+export FSLLOCKDIR=""
+export FSLMACHINELIST=""
+export FSLMULTIFILEQUIT="TRUE"
+export FSLOUTPUTTYPE="NIFTI_GZ"
+export FSLREMOTECALL=""
